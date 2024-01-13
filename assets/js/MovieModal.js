6015b7451c0d1a8ae73082d5bee409ac35bfed0a
HDanDev <dhan_nayphotmail.com>
2024-01-14 00:13:39 +0100

    polishing last details in css modal

--- assets/js/MovieModal.js
+++ assets/js/MovieModal.js
@@ -11,6 +11,10 @@ class MovieModal {
         const modal = document.getElementById(this.targetElementId);
         modal.innerHTML = '';
 
+        const backgroundImage = document.createElement('div');
+        backgroundImage.classList.add('modal-background');
+        backgroundImage.style.backgroundImage = `url("${movieData.image_url}")`;
+
         const modalCloseIcon = document.createElement('i');
         modalCloseIcon.id = 'closeModalBtn';
         modalCloseIcon.classList.add('icon-cross');
@@ -28,7 +32,7 @@ class MovieModal {
         imdbRatingBlock.classList.add('modal-imdb-rating-block');
 
         const imdbRating = document.createElement('p');
-        imdbRating.innerText = movieData.imdb_score;
+        imdbRating.innerText = `Imdb score: ${movieData.imdb_score}`;
 
         const imdbRatingStarIcon = document.createElement('i');
         imdbRatingStarIcon.classList.add('icon-star');
@@ -38,6 +42,7 @@ class MovieModal {
 
         const rating = document.createElement('p');
         rating.innerText = movieData.rated;
+        rating.classList.add('rating');                
         switch (movieData.rated) {
             case 'G':
                 rating.classList.add('green');                
@@ -58,22 +63,22 @@ class MovieModal {
 
         const movieGenre = document.createElement('p');
         const genresString = movieData.genres.join(', ');
-        movieGenre.innerText = genresString;
+        movieGenre.innerText = `Genre: ${genresString}`;
 
         const releaseDate = document.createElement('p');
-        releaseDate.innerText = movieData.date_published;
+        releaseDate.innerText = `Release date: ${movieData.date_published}`;
 
         const directors = document.createElement('p');
         const directorsString = movieData.directors.join(', ');
-        directors.innerText = directorsString;
+        directors.innerText = `Director: ${directorsString}`;
 
         const actors = document.createElement('p');
         const actorsString = movieData.actors.join(', ');
-        actors.innerText = actorsString;
+        actors.innerText = `Actors: ${actorsString}`;
 
         const country = document.createElement('p');
         const countriesString = movieData.countries.join(', ');
-        country.innerText = countriesString;
+        country.innerText = `Countries: ${countriesString}`;
 
         const duration = movieData.duration;
         const hours = Math.floor(duration / 60);
@@ -82,14 +87,22 @@ class MovieModal {
         const durationString = `${hours}h${formattedMinutes}`;
 
         const movieDuration = document.createElement('p');
-        movieDuration.innerText = durationString;
+        movieDuration.innerText = `Duration: ${durationString}`;
         
         const boxOffice = document.createElement('p');
-        const formattedAmount = movieData.worldwide_gross_income != null ? `${movieData.worldwide_gross_income.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ')}$` : 'Not disclosed';
-        boxOffice.innerText = formattedAmount;
+        const formattedAmount = movieData.worldwide_gross_income != null ? `$${movieData.worldwide_gross_income.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ')}` : 'Not disclosed';
+        boxOffice.innerText = `Box office: ${formattedAmount}`;
 
         const description = document.createElement('p');
         description.innerText = movieData.description;
+
+        const playButton = document.createElement('button');
+        playButton.classList.add('modal-btn');
+        playButton.innerText = "Play "
+        const playIcon = document.createElement('i');
+        playIcon.classList.add('icon-play');
+    
+        playButton.appendChild(playIcon);
 
         descriptionBlock.appendChild(imdbRatingBlock);
         if (movieData.rated != 'Not rated or unkown rating') descriptionBlock.appendChild(rating);
@@ -100,7 +113,6 @@ class MovieModal {
         descriptionBlock.appendChild(country);
         descriptionBlock.appendChild(movieDuration);
         descriptionBlock.appendChild(boxOffice);
-        descriptionBlock.appendChild(description);
     
         const imgBlock = document.createElement('div');
         imgBlock.classList.add('modal-img-block');
@@ -111,16 +123,19 @@ class MovieModal {
         img.classList.add('modal-img');   
     
         imgBlock.appendChild(img);
-        img.onerror = ()=>{img.src = '../assets/img/JSI_logo_only_w.png'; img.classList.add('broken-img');}
+        img.onerror = ()=>{img.src = '../assets/img/JSI_logo_only_w.png'; img.classList.add('modal-broken-img'); img.classList.remove('modal-img');}
         
         detailsBlock.appendChild(descriptionBlock);
         detailsBlock.appendChild(imgBlock);
     
         // img.addEventListener('click', () => displayImgLightBox(img));
-    
+        
+        modal.appendChild(backgroundImage);
         modal.appendChild(modalCloseIcon);
         modal.appendChild(movieTitleBlock);
+        modal.appendChild(description);
         modal.appendChild(detailsBlock);
+        modal.appendChild(playButton);
     
         this.openModal();
     }
